baef69202067b827777a115a08bc835c4d338875
Matheus Mello <mellomatheuslima@gmail.com>
2018-08-26 19:28:28 -0300

    Alterando a forma como o bot printa as informacoes do bolsafamilia

--- commands/transparency/BolsaFamiliaCommand.js
+++ commands/transparency/BolsaFamiliaCommand.js
@@ -1,4 +1,5 @@
 const Commando = require("discord.js-commando");
+const Discord = require("discord.js");
 const GovernInterfaceApi = require("../../utils/models/GovernInterfaceApi");
 const HttpService = require("../../utils/services/HttpService");
 const ibge = require("../../utils/helpers/ibge");
@@ -20,9 +21,15 @@ class BolsaFamiliaCommand extends Commando.Command {
     }
 
     async run(msg, args) {
+        let answer = new Discord.RichEmbed()
+            .setTitle("Resultado da Pesquisa")
+            .setColor(0x99e6ff)
+            .setFooter("Este serviço utiliza a API de Dados do Portal de Transparência do Governo Federal localizado em: http://www.transparencia.gov.br");
         let arrayArgs = args.trim().split(/ +/g);
         if (arrayArgs.length <= 1) {
-            msg.reply("Faltam parametros para pesquisa.\nExemplo de pesquisa: !bolsafamilia 201805 RioDeJaneiro");
+            answer.addField("Error", "Faltam parametros para pesquisa");
+            answer.addField("Exemplo de pesquisa", "!bolsafamilia 201805 RioDeJaneiro");
+            msg.channel.send(answer);
             return;
         }
 
@@ -34,9 +41,16 @@ class BolsaFamiliaCommand extends Commando.Command {
 
         this.httpService.get(this.bolsaFamiliaApi.url).then(response => {
             let res = response[0];
-            msg.channel.send(`Programa do Bolsa Família\nData de Referência: ${res.dataReferencia}.\nMunicípio: ${res.municipio.nomeIBGE}.\nValor: ${res.valor}.\nQuantidade de beneficiados: ${res.quantidadeBeneficiados}.\nURL do Serviço: ${this.bolsaFamiliaApi.url}`);
+            answer.addField("Data de Referência", res.dataReferencia);
+            answer.addField("Município", res.municipio.nomeIBGE);
+            answer.addField("Valor", res.valor);
+            answer.addField("Quantidade de beneficiados", res.quantidadeBeneficiados);
+            answer.addField("URL do Serviço", this.bolsaFamiliaApi.url);
+            // answer.setDescription(`Programa do Bolsa Família\nData de Referência: ${res.dataReferencia}.\nMunicípio: ${res.municipio.nomeIBGE}.\nValor: ${res.valor}.\nQuantidade de beneficiados: ${res.quantidadeBeneficiados}.\nURL do Serviço: ${this.bolsaFamiliaApi.url}`);
+            msg.channel.send(answer);
         }).catch(err => {
-            msg.reply("Não foi possível realizar a pesquisa na serviço de transparência do governo!");
+            answer.addField("Error", "Não foi possível realizar a pesquisa na serviço de transparência do governo!");
+            msg.channel.send(answer);
         });
     }
 }
