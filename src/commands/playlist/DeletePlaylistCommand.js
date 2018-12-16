40bc65cf667c35b9cc407ac27bbed7994d409e15
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2018-12-16 20:47:29 -0200

    Implementacao do comando de deletar playlists

---
+++ src/commands/playlist/DeletePlaylistCommand.js
@@ -0,0 +1,55 @@
+const Commando = require("discord.js-commando");
+const Discord = require("discord.js");
+const Playlist = require("../../models/Playlist.js");
+const PlaylistController = require("../../controllers/PlaylistController.js");
+
+class DeletePlaylistCommand extends Commando.Command {
+
+    constructor(client) {
+        super(client, {
+            name: "delplaylist",
+            group: "playlist",
+            memberName: "delplaylist",
+            description: "Exclui uma playlist do servidor."
+        });
+    }
+
+    async run(msg, args) {
+        let answer = new Discord.RichEmbed();
+        if (!args) {
+            answer
+                .setTitle("Parametro do comando inválido.")
+                .addField("Exemplo:", `\`!delplaylist <nome-da-playlist>\`.`);
+            return msg.channel.send(answer);
+        }
+
+        let arrayArgs = args.trim().split(/ +/g);
+
+        let playlistName;
+        if (arrayArgs.length > 0) {
+            playlistName = arrayArgs[0];
+        }
+
+        if (!playlistName) {
+            return msg.channel.send("Parametros para exclusão de playlist inválidos.");
+        }
+
+        PlaylistController.deletePlaylistByaName(playlistName, msg.guild.id)
+            .then((playlist) => {
+                answer
+                    .setTitle("Playlist excluída com sucesso.")
+                    .addField("ID da playlist:", `\`${playlist._id}\``)
+                    .addField("Nome da playlist:", `\`${playlist.name}\`.`)
+                    .addField(`Quantidade de músicas:`, `\`${playlist.songs.length}\`.`);
+
+                msg.channel.send(answer);
+            })
+            .catch((error) => {
+                console.log(error);
+                msg.channel.send("Não foi possível excluir esta playlist.");
+            });
+    }
+
+}
+
+module.exports = DeletePlaylistCommand;
--- src/controllers/PlaylistController.js
+++ src/controllers/PlaylistController.js
@@ -14,6 +14,24 @@ class PlaylistController {
             });
         });
     }
+
+    static deletePlaylistByaName(playlistName, guildId) {
+        return new Promise((resolve, reject) => {
+            db.playlists.findOne({ name: playlistName, guildId: guildId }, (error, playlist) => {
+                if (error) {
+                    return reject(error);
+                }
+
+                db.playlists.remove({ name: playlistName, guildId: guildId }, (error, deleteInfo) => {
+                    if (error) {
+                        return reject(error);
+                    }
+    
+                    resolve(playlist);
+                });
+            });
+        });
+    }
 }
 
 module.exports = PlaylistController;
