733b8845eb72da181f9b447ddc71efb47220552e
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2018-12-16 14:06:16 -0200

    Removendo classes de manipulacao de banco de dados mysql

--- common/TableDocument.js
+++
@@ -1,9 +0,0 @@
-class TableDocument {
-    constructor(constructorObj) {
-        Object.keys(constructorObj).forEach(objKey => {
-            this[objKey.toCamelCase()] = constructorObj[objKey];
-        });
-    }
-}
-
-module.exports = TableDocument;
