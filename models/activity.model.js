1ab473332853e971ee4fa73c4cd654ef58e0bcbe
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-07-03 21:39:28 -0300

    Change used to Boolean and Add timestamps

--- models/activity.model.js
+++ models/activity.model.js
@@ -1,14 +1,19 @@
 const mongoose = require('../database');
 
-const ActivitySchema = new mongoose.Schema({
-  description: {
-    type: String,
-    required: true,
+const ActivitySchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+    },
+    used: {
+      type: Boolean,
+      required: true,
+    },
   },
-  used: {
-    type: String,
-    required: true,
+  {
+    timestamps: true,
   },
-});
+);
 
 module.exports = mongoose.model('Activity', ActivitySchema);
