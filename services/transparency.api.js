487986024c46e8586a12db5a1a6e0c563a208be3
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-07-07 20:21:32 -0300

    Use the new config string to service base url

--- services/transparency.api.js
+++ services/transparency.api.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
+const config = require('../config');
 
 module.exports = axios.create({
-  baseURL: 'http://www.transparencia.gov.br/api-de-dados',
+  baseURL: config.services.api.transparency.baseUrl,
 });
