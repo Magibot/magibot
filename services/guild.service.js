fcf5ac48b246ce7940e5e85d8de0c2edbbd12e91
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-10-31 17:05:21 -0300

    create guild service for handling http request to api

---
+++ services/guild.service.js
@@ -0,0 +1,30 @@
+const superagent = require('../config/superagent');
+const env = require('../config/env');
+
+const create = async guild => {
+  try {
+    const response = await superagent
+      .post('/guilds')
+      .send(guild)
+      .set('Authorization', env.apiClientId)
+      .set('authorization_type', 'client_id');
+
+    return {
+      status: 'success',
+      code: response.statusCode,
+      payload: response.body.payload
+    };
+  } catch (err) {
+    // console.log(err);
+    return {
+      status: 'error',
+      type: err.name,
+      code: err.response.statusCode,
+      errors: err.response.body.errors
+    };
+  }
+};
+
+module.exports = {
+  create
+};
