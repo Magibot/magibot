22a31667d06ce00505eefe6a7aa03ad823b2623c
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-11-12 15:57:29 -0300

    Setup new env var

--- config/env.js
+++ config/env.js
@@ -1,28 +1,28 @@
-require('dotenv').config();
+require("dotenv").config();
 
 let token;
-if (process.env.NODE_ENV === 'production') {
-  token = process.env.TOKEN;
+if (process.env.NODE_ENV === "production") {
+  token = process.env.DISCORD_TOKEN;
 } else {
-  token = process.env.DEV_TOKEN;
+  token = process.env.DISCORD_DEV_TOKEN;
 }
 
 if (!token) {
   console.error(
-    'No token provided. Bot application need a token to handle Discord connection'
+    "No Discord token provided. Bot application need a token to handle Discord connection"
   );
   process.exit(1);
 }
 
 const apiClientId = process.env.API_CLIENT_ID;
 if (!apiClientId) {
-  console.error('No Client ID registered in environment.');
+  console.error("No API Client ID registered in environment.");
   process.exit(1);
 }
 
 const apiBaseUrl = process.env.API_BASE_URL;
 if (!apiBaseUrl) {
-  console.error('No API base URL provided');
+  console.error("No API base URL provided");
   process.exit(1);
 }
 
