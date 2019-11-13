e4d9e01c00a39a91e2ad50e39b5d97a20fcfa2c5
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-11-13 13:38:54 -0300

    Set options in command

--- commands/utils/purge.js
+++ commands/utils/purge.js
@@ -2,42 +2,38 @@ const Commando = require('discord.js-commando');
 const logger = require('../../utils/logger');
 const env = require('../../config/env');
 
-const commandName = 'purge';
-
-const options = {
-  defaultHandling: false,
-  name: commandName,
-  group: 'utils',
-  memberName: commandName,
-  description: 'This command removes all messages from all users in the channel, minimum of 2, up to 100 messages',
-  details: 'Should receive one argument referring to the amount of messages you want to purge from channel',
-  examples: [
-    `${env.client.prefix} ${commandName} 50 (this will purge the last 50 messages from the channel)`,
-  ],
-  throttling: {
-    usages: 2,
-    duration: 3600,
-  },
-  guildOnly: true,
-  clientPermissions: ['ADMINISTRATOR'],
-  userPermissions: ['MANAGE_MESSAGES'],
-  argsCount: 1,
-  args: [
-    {
-      key: 'deleteCount',
-      prompt: '',
-      type: 'integer',
-      validate: (deleteCount) => deleteCount && deleteCount > 1 && deleteCount < 101,
-      label: 'amount of messages',
-    },
-  ],
-};
-
-exports.PurgeOptions = options;
-
 class Purge extends Commando.Command {
+  static options() {
+    return {
+      name: 'purge',
+      group: 'utils',
+      memberName: 'purge',
+      description: 'This command removes all messages from all users in the channel, minimum of 2, up to 100 messages',
+      details: 'Should receive one argument referring to the amount of messages you want to purge from channel',
+      examples: [
+        `${env.client.prefix} purge 50 (this will purge the last 50 messages from the channel)`,
+      ],
+      throttling: {
+        usages: 2,
+        duration: 3600,
+      },
+      guildOnly: true,
+      clientPermissions: ['ADMINISTRATOR'],
+      userPermissions: ['MANAGE_MESSAGES'],
+      args: [
+        {
+          key: 'deleteCount',
+          prompt: '',
+          type: 'integer',
+          validate: (deleteCount) => deleteCount && deleteCount > 1 && deleteCount < 101,
+          label: 'amount of messages',
+        },
+      ],
+    };
+  }
+
   constructor(client) {
-    super(client, options);
+    super(client, Purge.options());
   }
 
   async run(message, { deleteCount }) {
