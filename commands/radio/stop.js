c0277a898898dfd0e5f064dab0518d3084b5206b
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-11-21 13:25:57 -0300

    Add aliases to stop command

--- commands/radio/stop.js
+++ commands/radio/stop.js
@@ -7,6 +7,7 @@ class Stop extends Commando.Command {
       usage: `${env.client.prefix} stop`,
       name: 'stop',
       group: 'radio',
+      aliases: ['s', 'break'],
       memberName: 'stop',
       description: 'Stop the stream. This command will also clear the queue',
       details: 'Destroy the music if you hate it',
