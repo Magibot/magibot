73f90e6434a5ad632ffc6b48966d8fd00186715f
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-11-25 11:38:32 -0300

    Use nlp config file to setup dinamically

--- nlp.js
+++ nlp.js
@@ -1,24 +1,22 @@
 const { NlpManager } = require('node-nlp');
+const config = require('./nlp-config.json');
 
-const manager = new NlpManager({ languages: ['en'] });
+const languages = Object.keys(config);
+const manager = new NlpManager({ languages });
 
-// Adds the utterances and intents for the NLP
-manager.addDocument('en', 'goodbye for now', 'greetings.bye');
-manager.addDocument('en', 'bye bye take care', 'greetings.bye');
-manager.addDocument('en', 'okay see you later', 'greetings.bye');
-manager.addDocument('en', 'bye for now', 'greetings.bye');
-manager.addDocument('en', 'i must go', 'greetings.bye');
+languages.forEach((lang) => {
+  const types = Object.keys(config[lang]);
+  types.forEach((tp) => {
+    const { messages, answers } = config[lang][tp];
+    messages.forEach((msg) => {
+      manager.addDocument(lang, msg, tp);
+    });
 
-manager.addDocument('en', 'hello', 'greetings.hello');
-manager.addDocument('en', 'hi', 'greetings.hello');
-manager.addDocument('en', 'howdy', 'greetings.hello');
-
-// Train also the NLG
-manager.addAnswer('en', 'greetings.bye', 'Till next time');
-manager.addAnswer('en', 'greetings.bye', 'see you soon!');
-
-manager.addAnswer('en', 'greetings.hello', 'Hey there!');
-manager.addAnswer('en', 'greetings.hello', 'Greetings!');
+    answers.forEach((ans) => {
+      manager.addAnswer(lang, tp, ans);
+    });
+  });
+});
 
 module.exports = manager;
 
