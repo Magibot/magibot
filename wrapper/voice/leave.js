3e259e17ff7668125c9fc1c61d5ed275df95088e
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-11-27 19:37:01 -0300

    Create join and leave command options in a wrapper

---
+++ wrapper/voice/leave.js
@@ -0,0 +1,15 @@
+const options = (prefix) => ({
+  usage: `${prefix} leave`,
+  name: 'leave',
+  aliases: ['disconnect', 'out', 'go'],
+  group: 'voice',
+  memberName: 'leave',
+  description: 'Leave the voice channel if the bot is in one',
+  details: 'Come on man, don\'t threat me like that',
+  examples: [
+    `${prefix} leave`,
+  ],
+  guildOnly: true,
+});
+
+module.exports = options;
