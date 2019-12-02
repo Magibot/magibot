61b52fd940c599ae848d46b33c831e38a9b1cc24
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-12-02 18:28:59 -0300

    Fix wrapper to include subcommands

--- wrapper/index.js
+++ wrapper/index.js
@@ -1,38 +1,46 @@
 const fs = require('fs');
 const path = require('path');
+const config = require('../config/bot');
 
 const getDirectories = (source) => fs.readdirSync(source, { withFileTypes: true })
   .filter((dirent) => dirent.isDirectory())
   .map((dirent) => dirent.name);
 
+const requireCommandOptions = (requirePath) => {
+  const options = {};
+  const dirents = fs.readdirSync(requirePath, { withFileTypes: true });
+  dirents.forEach((dir) => {
+    if (dir.isFile()) {
+      const innerPath = path.join(requirePath, dir.name);
+      const [modulename, extension] = dir.name.split('.');
+      if (extension === 'js') {
+        const inner = require(innerPath);
+        const commandOptions = inner.options(config.env.discord.prefix);
+        if (modulename === 'index') {
+          const wrapperCommand = requirePath.split(path.sep).pop();
+          options[wrapperCommand] = commandOptions;
+          options[wrapperCommand].isWrapper = true;
+          options[wrapperCommand].subCommands = requireCommandOptions(path.join(requirePath, 'sub'));
+        } else {
+          options[modulename] = commandOptions;
+        }
+      }
+    }
+  });
+
+  return options;
+};
 
 const getCommandsOptions = () => {
-  const options = {};
+  let options = {};
   const directories = getDirectories(__dirname);
   directories.forEach((subDirName) => {
     const fullPath = path.join(__dirname, subDirName);
-    const dirents = fs.readdirSync(fullPath, { withFileTypes: true });
-
-    dirents.forEach((dir) => {
-      const innerPath = path.join(fullPath, dir.name);
-      if (dir.name === 'sub' && dir.isDirectory()) {
-        console.log(`Directory ${dir.name}`);
-        const subdirents = fs.readdirSync(innerPath, { withFileTypes: true });
-        subdirents.forEach((subdir) => {
-          console.log(subdir.name);
-        });
-      } else {
-        console.log(`File ${dir.name}`);
-        const [modulename, type] = dir.name.split('.');
-        if (type === 'js') {
-          const inner = require(innerPath);
-          options[modulename] = inner.options('!magi');
-        }
-      }
-    });
+    const commandOptions = requireCommandOptions(fullPath);
+    options = { ...options, ...commandOptions };
   });
 
-  console.log(options);
+  return options;
 };
 
 module.exports = {
