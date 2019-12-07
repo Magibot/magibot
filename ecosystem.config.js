40cd6529a8d15b3f80da2f4892f785b56762540e
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-12-06 23:31:02 -0300

    max memory pm2 restart

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
     instances: 'max',
     autorestart: true,
     watch: false,
-    max_memory_restart: '1G',
+    max_memory_restart: '512M',
     env: {
       NODE_ENV: 'development',
     },
