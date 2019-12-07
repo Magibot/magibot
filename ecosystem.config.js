226886283787d663497da9ed0f07593498608eff
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2019-12-06 23:36:06 -0300

    ajust instances pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'Magibot',
     script: './app.js',
-    instances: 'max',
+    instances: 2,
     autorestart: true,
     watch: false,
     max_memory_restart: '512M',
